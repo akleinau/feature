30d917c22151f62571ca423baca90d3218f86bc1
akleinau <56925445+akleinau@users.noreply.github.com>
2024-05-16 16:45:11 +0200

    refactor

--- functions.py
+++ functions.py
@@ -66,3 +66,13 @@ def calc_shap_values(data, background, model, columns, combined_columns=None):
 
 def detangled_predict(data, model, column_names):
     return model.predict(split_all_columns(data).reindex(columns=column_names))
+
+def get_shap(data, means, model, columns, classes):
+    all_shap_explanations = calc_shap_values(data, means, model, columns)
+    all_shap_values = pd.DataFrame(all_shap_explanations.values, columns= ['shap_' + name for name in all_shap_explanations.feature_names])
+    all_predictions = model.predict_proba(data)
+    all_predictions = pd.DataFrame(all_predictions, columns=['prob_' + str(name) for name in classes])
+    all_predictions['prediction'] = all_predictions.idxmax(axis=1)
+    #merge X_test, shap, predictions
+    all_data = pd.concat([data, all_shap_values, all_predictions], axis=1)
+    return all_data
