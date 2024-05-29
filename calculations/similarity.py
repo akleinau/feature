95ba9f91fb987e70c68e688531d7180e9f6460b1
akleinau <56925445+akleinau@users.noreply.github.com>
2024-05-29 14:46:58 +0200

    improve clustering

--- calculations/similarity.py
+++ calculations/similarity.py
@@ -1,3 +1,5 @@
+import pandas
+import pandas as pd
 from sklearn.tree import DecisionTreeRegressor
 from bokeh.palettes import Category20
 import numpy as np
@@ -99,11 +101,68 @@ def get_tree_rules(tree, feature_names):
             if rule != "":
                 rule += " and "
             rule += str(p)
+        if rule == "":
+            rule = "All"
+
         # get class name
         class_label = path[-1]
         rules[class_label] = rule
 
     return rules
+
+def _make_readable_axiom(axiom):
+    if (axiom['operator'] == '>' and axiom['value'] == '0.5'):
+        return 'Higher '
+    elif (axiom['operator'] == '<=' and axiom['value'] == '-0.5'):
+        return 'Lower '
+    elif (axiom['operator'] == '>' and axiom['value'] == '-0.5'):
+        return 'Similar/ Higher '
+    elif (axiom['operator'] == '<=' and axiom['value'] == '0.5'):
+        return 'Similar/ Lower '
+    else:
+        return 'Similar '
+
+def make_readable(x):
+    axioms = x.split(' and ')
+    axioms = [axiom.split() for axiom in axioms]
+    if len(axioms[0]) < 3:
+        return x
+
+    axioms = pd.DataFrame(axioms, columns=['feature', 'operator', 'value'])
+    new_rules = []
+    axioms['new_rule'] = axioms.apply(_make_readable_axiom, axis=1)
+
+    #if both'Similar/ Higher' and 'Similar/ Lower' exist for a feature, merge them together into 'Similar'
+    rule_groups = axioms.groupby('feature')
+    rule_list = []
+    for name, group in rule_groups:
+        if 'Similar/ Higher ' in group['new_rule'].values and 'Similar/ Lower ' in group['new_rule'].values:
+                rule_list.append('Similar ' + group['feature'].values[0])
+        else:
+            rule_list.extend(group['new_rule'].values + group['feature'].values)
+
+    #now merge the rules together
+    return ' and '.join(rule_list)
+
+def shorten_rules(x):
+    axioms = x.split(' and ')
+    axioms = [axiom[1:-1] for axiom in axioms]
+    axioms = [axiom.split() for axiom in axioms]
+    if len(axioms[0]) < 3:
+        return x
+
+
+    grouped_axioms = pd.DataFrame(axioms, columns=['feature', 'operator', 'value'])
+
+    #now group
+    grouped_axioms= grouped_axioms.groupby(['feature', 'operator'])
+    grouped_axioms = grouped_axioms['value'].agg(['max', 'min'])
+    grouped_axioms = grouped_axioms.reset_index()
+    grouped_axioms['value'] = grouped_axioms.apply(lambda x: x['max'] if x['operator'] == '>' else x['min'], axis=1)
+
+    #now create the new label
+    grouped_axioms = grouped_axioms['feature'] + ' ' + grouped_axioms['operator'] + ' ' + grouped_axioms['value']
+    return grouped_axioms.str.cat(sep=' and ')
 
 
 def get_tree_groups(data, all_selected_cols, cur_col, prediction, exclude_col=True):
@@ -126,6 +185,55 @@ def get_tree_groups(data, all_selected_cols, cur_col, prediction, exclude_col=Tr
         rules = get_tree_rules(tree, columns)
 
         data["scatter_label"] = data["group"].apply(lambda x: rules[x])
+        data["scatter_label"] = data["scatter_label"].apply(shorten_rules)
+    else:
+        data["scatter_group"] = '#228b22'
+        data["scatter_label"] = 'All'
+
+    return data
+
+def get_relative(x, item_val, range):
+    # lambda x: 'saddlebrown' if x >= item_val else 'midnightblue'
+    if (x < item_val - range):
+        return -1
+    elif (x > item_val + range):
+        return 1
+    else:
+        return 0
+
+def get_relative_tree_groups(data, all_selected_cols, cur_col, prediction, index, exclude_col=True):
+    # remove the current column from the list of all selected columns
+    if exclude_col:
+        columns = [col for col in all_selected_cols if col != cur_col]
+    else:
+        columns = all_selected_cols
+
+    if (len(columns) > 0):
+        #make new relative columns
+        item = data.iloc[index]
+        relative_data = pd.DataFrame()
+        for col in columns:
+            item_val = item[col]
+            range = data[col].max() - data[col].min()
+
+            relative_data[col] = data[col].apply(lambda x: get_relative(x, item_val, range / 20))
+
+
+
+        tree = DecisionTreeRegressor(max_leaf_nodes=4, max_depth=2, min_samples_leaf=0.1)
+        tree.fit(relative_data, data[prediction])
+
+        data["group"] = tree.apply(relative_data)
+
+        # create a color for each group
+        data["scatter_group"] = data["group"].apply(lambda x: Category20[20][x])
+
+        # create human-readable labels for each group containing the path to the group
+        rules = get_tree_rules(tree, columns)
+
+        data["scatter_label"] = data["group"].apply(lambda x: rules[x])
+        data["scatter_label"] = data["scatter_label"].apply(shorten_rules)
+        data["scatter_label"] = data["scatter_label"].apply(make_readable)
     else:
         data["scatter_group"] = '#228b22'
         data["scatter_label"] = 'All'
@@ -135,6 +243,6 @@ def get_tree_groups(data, all_selected_cols, cur_col, prediction, exclude_col=Tr
 
 def get_clustering(cluster_type, data, all_selected_cols, cur_col, prediction, index, exclude_col=True):
     if cluster_type == 'Relative':
-        return get_relative_groups(data, all_selected_cols, index)
+        return get_relative_tree_groups(data, all_selected_cols, cur_col, prediction, index, exclude_col)
     else:
         return get_tree_groups(data, all_selected_cols, cur_col, prediction, exclude_col)
