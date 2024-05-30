0c1ced6af76d1d752c76d9d9298fc1ed8483e136
akleinau <56925445+akleinau@users.noreply.github.com>
2024-05-30 13:47:05 +0200

    refactor classes

--- DataStore.py
+++ DataStore.py
@@ -9,34 +9,35 @@ from calculations import column_functions, similarity
 
 
 class DataStore(Viewer):
-    # data = param.DataFrame()
-    file_widget = None
-    nn_widget = None
 
     def __init__(self, **params):
         super().__init__(**params)
         self.file_widget = pn.widgets.FileInput(accept='.csv', name='Upload data')
         self.nn_widget = pn.widgets.FileInput(accept='.pkl', name='Upload neural network')
+        self.nn = pn.bind(data_loader.load_nn, self.nn_widget, self.file_widget)
         # self.data = pn.bind(data_loader.load_data, self.file_widget.rx, self.nn_widget.rx)
-        self.x = pn.widgets.EditableIntSlider(name='x', start=0, end=100, value=26)
-        self.col = pn.widgets.Select(name='column', options=self.get_columns())
-        # self.cur_feature = pn.widgets.Select(name='', options=self.get_all_selected_cols(), align='center')
+        self.x_widget = pn.widgets.EditableIntSlider(name='x', start=0, end=100, value=26)
+        self.x = self.x_widget.rx()
+        self.data = self.get_data()
+        self.raw_data = pn.bind(data_loader.load_data, self.file_widget, self.nn_widget)
+        self.classes = pn.bind(lambda nn: nn.classes_, self.nn)
+        self.means = pn.bind(feature.get_means, self.raw_data)
+        
+        #columns
+        self.columns = pn.bind(lambda data: [col for col in data.columns], self.raw_data)
+        self.col = pn.widgets.Select(name='column', options=self.columns)
+        self.all_selected_cols = pn.bind(column_functions.return_col, self.col)
+
+        #groups
+        self.cur_feature = pn.widgets.Select(name='', options=self.all_selected_cols, align='center')
         self.column_group = []
         self.combined_columns = pn.widgets.LiteralInput(value=[])
         self.num_groups = pn.widgets.LiteralInput(value=1)
-        self.remaining_options = pn.widgets.LiteralInput(value=self.get_columns())
-        self.row = pn.FlexBox().servable()
-
-        # update everything when the data changes
-        self.get_file_widget().param.watch(
-            lambda event: data_loader.data_changed(event,
-           [self.get_col(), self.get_cur_feature(), self.get_all_selected_cols(), self.get_widget]),
-                    parameter_names=['value'], onlychanged=False)
-        self.get_nn_widget().param.watch(lambda event: data_loader.data_changed(event,
-            [self.get_col(), self.get_cur_feature(), self.get_all_selected_cols(), self.get_widget]),
-                    parameter_names=['value'], onlychanged=False)
+        self.remaining_options = pn.widgets.LiteralInput(value=self.columns)
+        self.row = pn.FlexBox()
 
 
+        #customization widgets
         self.cluster_type = pn.widgets.Select(name='cluster_type', options=['Relative Decision Tree', 'Decision Tree'],
                                          value='Relative Decision Tree')
         self.chart_type = pn.widgets.MultiChoice(name='chart_type', options=['scatter', 'line', 'band', 'contour'],
@@ -44,7 +45,40 @@ class DataStore(Viewer):
         self.graph_type = pn.widgets.Select(name='graph_type', options=['Cluster', 'Dependency', 'Parallel'],
                                        value='Cluster')
 
+
+        # update everything when the data changes
+        self.file_widget.param.watch(
+            lambda event: data_loader.data_changed(event,
+           [self.col, self.cur_feature, self.all_selected_cols, self.get_widget]),
+                    parameter_names=['value'], onlychanged=False)
+        self.nn_widget.param.watch(lambda event: data_loader.data_changed(event,
+            [self.col, self.cur_feature, self.all_selected_cols, self.get_widget]),
+                    parameter_names=['value'], onlychanged=False)
+
+
+
         column_functions.init_groups(self.get_widget())
+        self.data_and_probabilities = pn.bind(feature.combine_data_and_results, self.data, self.nn, self.classes)
+
+        #item
+        self.item_prediction = pn.bind(item_functions.get_item_prediction, self.data_and_probabilities, self.x)
+        self.item_shap = pn.bind(item_functions.get_item_shap_values, self.data(), self.x, self.means, self.nn,
+                        self.columns, self.combined_columns)
+
+        self.item_data = pn.bind(item_functions.get_item_data, self.data, self.x)
+        self.prob_data = pn.bind(item_functions.get_item_probability_string, self.data_and_probabilities, self.x,
+                       self.item_prediction)
+        self.prob_wo_selected_cols = pn.bind(item_functions.get_prob_wo_selected_cols, self.nn, self.all_selected_cols,
+                        self.means, self.item_data, self.item_prediction)
+
+        #clustered data
+        self.clustered_data = pn.bind(similarity.get_clustering, self.cluster_type, self.data_and_probabilities,
+                        self.all_selected_cols, self.cur_feature, self.item_prediction, self.x,
+                        exclude_col=False)
+
+
+
+
 
     def get_all_data(self):
         return pn.bind(data_loader.load_data, self.file_widget.value, self.nn_widget.value)
@@ -55,85 +89,12 @@ class DataStore(Viewer):
     def get_file_widgets(self):
         return pn.Row(self.file_widget, self.nn_widget, self.x).servable()
 
-    def get_file_widget(self):
-        return self.file_widget
-
-    def get_nn_widget(self):
-        return self.nn_widget
-
-    def get_raw_data(self):
-        return pn.bind(data_loader.load_data, self.file_widget, self.nn_widget)
-
-    def get_nn(self):
-        return pn.bind(data_loader.load_nn, self.nn_widget, self.file_widget)
-
-    def get_means(self):
-        return pn.bind(feature.get_means, self.get_raw_data())
-
-    def get_classes(self):
-        return pn.bind(lambda nn: nn.classes_, self.get_nn())
-
-    def get_columns(self):
-        return pn.bind(lambda data: [col for col in data.columns], self.get_raw_data())
-
-    def get_data_and_probabilities(self):
-        return pn.bind(feature.combine_data_and_results, self.get_data(), self.get_nn(), self.get_classes())
-
-    def get_x(self):
-        return self.x.value
-
-    def get_item_prediction(self):
-        return pn.bind(item_functions.get_item_prediction, self.get_data_and_probabilities(), self.get_x())
-
-    def get_prob_data(self):
-        return pn.bind(item_functions.get_item_probability_string, self.get_data_and_probabilities(), self.get_x(),
-                       self.get_item_prediction())
-
-    def get_item_data(self):
-        return pn.bind(item_functions.get_item_data, self.get_data(), self.get_x())
-
-    def get_col(self):
-        return self.col.value
-
-    def get_all_selected_cols(self):
-        return pn.bind(column_functions.return_col, self.get_col())
-
-    def get_cur_feature(self):
-        return pn.bind(lambda a: a[0], self.get_all_selected_cols())  # self.cur_feature.value
-
-    def get_column_group(self):
-        return self.column_group
-
-    def get_combined_columns(self):
-        return self.combined_columns.value
-
-    def get_num_groups(self):
-        return self.num_groups.value
-
-    def get_remaining_options(self):
-        return self.remaining_options.value
-
-    def get_row(self):
-        return self.row
-
     def get_widget(self):
         return [self.column_group, self.row, self.num_groups, self.remaining_options, self.combined_columns,
-                self.get_columns()]
-
-
-    def get_item_shap(self):
-        return pn.bind(item_functions.get_item_shap_values, self.get_data(), self.get_x(), self.get_means(), self.get_nn(),
-                        self.get_columns(), self.get_combined_columns())
-
-
-    def get_prob_wo_selected_cols(self):
-        return pn.bind(item_functions.get_prob_wo_selected_cols, self.get_nn(), self.get_all_selected_cols(),
-                        self.get_means(), self.get_item_data(), self.get_item_prediction())
-
-    def get_clustered_data(self):
-        return pn.bind(similarity.get_clustering, self.cluster_type, self.get_data_and_probabilities(),
-                        self.get_all_selected_cols(), self.get_cur_feature(), self.get_item_prediction(), self.get_x(),
-                        exclude_col=False)
+                self.columns]
 
     def get_customization_widgets(self):
         return pn.Row(self.cluster_type, self.graph_type, self.chart_type).servable()
+
+    def get_row_widgets(self):
+        return self.row.servable()
--- feature_UI.py
+++ feature_UI.py
@@ -16,31 +16,30 @@ ds = DataStore()
 
 # create widgets
 ds.get_file_widgets()
-
 ds.get_customization_widgets()
+ds.get_row_widgets()
 
 # create all the widgets and variables needed for the column group selection
 pn.panel("<br>").servable()
 pn.panel("### Grouped columns:").servable()
 
 # shap plot
-shap_plot = pn.bind(shap_tornado_plot, ds.get_item_shap(), [ds.get_col()])  # col is wrapped to be passed as reference
+shap_plot = pn.bind(shap_tornado_plot, ds.item_shap, [ds.col])  # col is wrapped to be passed as reference
 
 # dependency plot
-
-dep_plot = pn.bind(dependency_scatterplot, ds.get_clustered_data(), ds.get_cur_feature(), ds.get_all_selected_cols(),
-                   ds.get_item_prediction(),ds.get_x(), ds.chart_type, ds.get_prob_wo_selected_cols())
+dep_plot = pn.bind(dependency_scatterplot, ds.clustered_data, ds.cur_feature, ds.all_selected_cols,
+                   ds.item_prediction,ds.x, ds.chart_type, ds.prob_wo_selected_cols)
 
 # parallel plot
-parallel_plot = pn.bind(parallel_plot, ds.get_clustered_data(), ds.get_cur_feature(), ds.get_all_selected_cols(),
-                        ds.get_item_prediction(), ds.get_item_data(), ds.chart_type)
+parallel_plot = pn.bind(parallel_plot, ds.clustered_data, ds.cur_feature, ds.all_selected_cols,
+                        ds.item_prediction, ds.item_data, ds.chart_type)
 
 # cluster bar plot
-cluster_plot = pn.bind(cluster_bar_plot, ds.get_clustered_data(), ds.get_cur_feature(), ds.get_all_selected_cols(),
-                       ds.get_item_prediction(),ds.get_x(), ds.chart_type, ds.get_prob_wo_selected_cols())
+cluster_plot = pn.bind(cluster_bar_plot, ds.clustered_data, ds.cur_feature, ds.all_selected_cols,
+                       ds.item_prediction,ds.x, ds.chart_type, ds.prob_wo_selected_cols)
 
 # remaining layout
-pn.pane.Str(ds.get_prob_data(), sizing_mode="stretch_width", align="center",
+pn.pane.Str(ds.prob_data, sizing_mode="stretch_width", align="center",
             styles={"font-size": "20px", "text-align": "center"}).servable()
 
 
@@ -52,7 +51,7 @@ def render_plot(graph_type, dep_plot, cluster_plot, parallel_plot, all_selected_
     else:
         return parallel_plot
 
-rendered_plot = pn.bind(render_plot, ds.graph_type, dep_plot, cluster_plot, parallel_plot, [ds.get_cur_feature()])
-pn.Row(ds.get_item_data(), shap_plot, rendered_plot).servable()
-#ds.get_cur_feature().servable()
+rendered_plot = pn.bind(render_plot, ds.graph_type, dep_plot, cluster_plot, parallel_plot, [ds.cur_feature])
+pn.Row(ds.item_data, shap_plot, rendered_plot).servable()
+#ds.cur_feature.servable()
 
